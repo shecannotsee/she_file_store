eb2e53d781b66afe5585307d77dff8236e3cc454
shecannotsee <shecannotsee@163.com>
2023-12-20 14:24:43 +0800

    [feat] build framework

---
+++ cmake/she_file_store-config.cmake
@@ -0,0 +1,13 @@
+
+# ${she_file_store_FOUND}       : right get
+# ${she_file_store_INCLUDE_DIR} : include
+# ${she_file_store_LIBRARIES}   : lib
+# ${she_file_store_LINK_TARGET} : link target
+
+set(she_file_store "she_file_store")
+set(${she_file_store}_FOUND "1")
+set(${she_file_store}_INCLUDE_DIR   "${CMAKE_PREFIX_PATH}/include")
+set(${she_file_store}_LIBRARIES     "${CMAKE_PREFIX_PATH}/lib")
+set(${she_file_store}_LINK_TARGET   "she_file_store")
+
+
